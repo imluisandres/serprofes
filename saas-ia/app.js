f9d2d0118c304fc47fd19ddd9120d17782bde5c4
imluisandres <luisandreslopez123@gmail.com>
2026-05-08 19:35:47 +0200

    Add app.js saas-ia

---
+++ saas-ia/app.js
@@ -0,0 +1,19 @@
+function enviarPrompt(event) {
+    // Evitamos que el form recargue la página
+    event.preventDefault();
+
+    //1. Capturar el texto
+
+    let mensaje = document.getElementById('mensaje-input').value.trim();
+
+    //2. Condicional
+
+    if (mensaje === "") {
+        alert("¡Error! Escribe algo primero")
+    } else {
+        alert("Mensaje recibido:\n" + mensaje);
+        //3. Limpiar input
+        document.getElementById('mensaje-input').value="";
+    }
+
+}
