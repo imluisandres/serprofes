34c130e10391009ece95824c0d0c50e19b588153
imluisandres <luisandreslopez123@gmail.com>
2026-05-11 22:02:00 +0200

    Update Cesta de la Compra 20260511

--- 20260511/02-ejercicio/script.js
+++ 20260511/02-ejercicio/script.js
@@ -1,4 +1,16 @@
-//1. Creamos nuestra "caja grande" (Array)
-let carrito = ["🍏 Manzana","🍌 Banana"];
-//2.Imprimir el carrito nada más al cargar la página
-document.getElementById('pantalla-cesta').textContent = carrito.join(" - ");
+//Creamos nuestra "Caja grande" (Array)
+let carrito =["🍏 Manzana "," 🍌 Platano "];
+//Imprimir el carrito nada mas al cargar la pagina
+
+document.getElementById('pantalla-cesta').textContent = carrito.join("-");
+//La funcion para agregar cosas nuevas a la lista
+function AñadirAlCarrito(){
+    //Atrapamos lo que escribe el usuario
+    let item = document.getElementById('nuevo-item').value;
+    //Magia de Array .push() mete el nuevo elemento al final de la lista
+    carrito.push(item);
+    //Volvemos a pintar la cesta para que se ariaValueMax
+    document.getElementById('pantalla-cesta').textContent = carrito.join("-");
+    //Limpiamos el input
+    document.getElementById('nuevo-item').value ="";
+}
